3e42755755d4c07c9a8a0df6ecb30899def6d2c7
Vincent-Wrck <61833578+Vincent-Wrck@users.noreply.github.com>
2020-09-11 12:26:04 +0200

    Update photoHover.js

--- js/photoHover.js
+++ js/photoHover.js
@@ -1,42 +1,44 @@
 $(document).ready(function () {
 
-    $(".photoHover")
+    let photoHover = $(".photoHover");
+    let isHoverActive = false;
+    $(photoHover).hover(function () {
+        isHoverActive = true;
+        setTimeout(function () {
+            if (isHoverActive) {
+                $("img").css({
+                    transform: "translateY(0%)",
+                    opacity: 0.8,
+                });
+                $("h2").css({
+                    transform: "translateY(100%)",
+                    opacity: 0,
+                });
+                $(".photoInfo").css({
+                    transform: "translateX(0%)",
+                    opacity: 1
+                });
+            }
+        }, 300);
+        }, function () {
 
-        .mouseenter(function () {
+        isHoverActive = false;
 
-            $("img").css({
-                transform: "translateY(0%)",
-                opacity: 0.8,
-            });
+        $("img").css({
+            opacity: 0,
+            transform: "translateY(100%)",
+        });
 
-            $("h2").css({
-                transform: "translateY(100%)",
-                opacity: 0,
-            });
+        $("h2").css({
+            transform: "translateY(0%)",
+            opacity: 1,
+        });
 
-            $(".photoInfo").css({
-                transform: "translateX(0%)",
-                opacity: 1
-            });
+        $(".photoInfo").css({
+            transform: "translateX(-100%)",
+            opacity: 0,
+        });
 
-        })
+    });
 
-        .mouseleave(function () {
-
-            $("img").css({
-                opacity: 0,
-                transform: "translateY(100%)",
-            });
-
-            $("h2").css({
-                transform: "translateY(0%)",
-                opacity: 1,
-            });
-
-            $(".photoInfo").css({
-                transform: "translateX(-100%)",
-                opacity: 0,
-            });
-
-        })
 });
