5b1823eacc0ad9aff4654efe92f6e44ca8228ecb
Vincent-Wrck <61833578+Vincent-Wrck@users.noreply.github.com>
2021-04-14 14:21:15 +0200

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,38 +1,17 @@
 $(document).ready(function () {
-
-
     $(".container").css({
-
         cursor: "none",
-
     });
-
     $(document).mousemove(function (event) {
-
         var mouseX = event.pageX;
         var mouseY = event.pageY;
-
         $(".cursor").css({
             top: mouseY,
             left: mouseX,
         });
-
         $(".cursorDelay").css({
             top: mouseY,
             left: mouseX,
         });
-
-
-//
-//        var artefact = setInterval(function () {
-//            $(".cursorArtefact").css({
-//
-//                top: (mouseY - 5),
-//                left: (mouseX - 5),
-//            });
-//
-//        }, 5)
-
     });
-
 });
