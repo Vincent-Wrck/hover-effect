023f7b1dd6e1215a8d80a9d259f843ad7738546d
Vincent-Wrck <61833578+Vincent-Wrck@users.noreply.github.com>
2021-04-14 14:23:01 +0200

    Update txtHover.js

--- js/txtHover.js
+++ js/txtHover.js
@@ -1,30 +1,16 @@
 $(document).ready(function () {
     
      $(".title").mouseover(function () {
-
             var colorArray = ["#13B3F2", "#17A655", "#D9D5D2", "#8C4014"];
             var randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-
             $(this).css({
-
                 color: randomColor,
                 cursor: "none",
                 transition: "300ms ease",
-
             });
-
             $(".cursor").css({
-
                 borderColor: randomColor,
 
             });
-         
-//            $(".cursorArtefact").css({
-//                
-//                background: randomColor,
-//                
-//            })
-
         });
-
 });
